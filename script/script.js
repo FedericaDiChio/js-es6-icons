73d263e87f6c19a1d4831872637794b2cb892a3b
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-26 16:50:24 +0200

    offset

--- script/script.js
+++ script/script.js
@@ -10,16 +10,17 @@ Creiamo una select con i tipi di icone e usiamola per filtrare le icone;
 
 // 1. Creiamo il template delle card;
 // 2. Creiamo una funzione che ci permetta di stampare il template; 
-// 3. Facciamo in modo che abbia anche l'Offset;
 const renderIcons = (icons, targetElement) => {
-let iconsTemplate = "";
-icons.forEach((icon) => {
+    let iconsTemplate = "";
+    icons.forEach((icon, index) => {        
+    // 3. Facciamo in modo che abbia anche l'Offset;
+    const hasOffset = index % 5 === 0 ? `offset-md-1` : " ";
     iconsTemplate += `
-    <div class="col-2 text-center">
+    <div class="col col-sm-4 col-md-2 ${hasOffset}">
         <div class="card">
             <div class="card-body">
                 <i class="${icon.family} ${icon.prefix}${icon.name}"></i>
-                <div class="h6">${icon.name}</div>
+                <div class="h6">${icon.name.toUpperCase()}</div>
             </div>
         </div>
     </div>`
@@ -29,6 +30,6 @@ targetElement.innerHTML = iconsTemplate;
 }
 
 // 3.Recuperiamo l'elemento HTML;
-const cardSection = document.querySelector("#icons, .row");
+const cardIcons = document.querySelector(".row");
 // 4. Chiamiamo la funzione;
-renderIcons(icons, cardSection);
+renderIcons(icons, cardIcons);
