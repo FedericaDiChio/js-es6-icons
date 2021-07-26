1eda0b918bdb3b00e6c567153327d6ec8a8011e9
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-26 15:59:55 +0200

    template card

--- script/script.js
+++ script/script.js
@@ -6,3 +6,29 @@ Coloriamo le icone per tipo;
 Milestone 3
 Creiamo una select con i tipi di icone e usiamola per filtrare le icone;
 */
+
+
+// 1. Creiamo il template delle card;
+// 2. Creiamo una funzione che ci permetta di stampare il template; 
+// 3. Facciamo in modo che abbia anche l'Offset;
+const renderIcons = (icons, targetElement) => {
+let iconsTemplate = "";
+icons.forEach((icon) => {
+    iconsTemplate += `
+    <div class="col-2 text-center">
+        <div class="card">
+            <div class="card-body">
+                <i class="${icon.family} ${icon.prefix}${icon.name}"></i>
+                <div class="h6">${icon.name}</div>
+            </div>
+        </div>
+    </div>`
+})
+// Elemento HTML 
+targetElement.innerHTML = iconsTemplate;
+}
+
+// 3.Recuperiamo l'elemento HTML;
+const cardSection = document.querySelector("#icons, .row");
+// 4. Chiamiamo la funzione;
+renderIcons(icons, cardSection);
